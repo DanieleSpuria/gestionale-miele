2dbf5562b591bd7b57a5fe66aeb06223aa61f503
DanieleSpuria <spuria.daniele@proton.me>
2023-11-28 16:20:29 +0100

    first commit

---
+++ app/Http/Controllers/BowlController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bowl;
+use Illuminate\Http\Request;
+
+class BowlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bowl  $bowl
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bowl $bowl)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bowl  $bowl
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bowl $bowl)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bowl  $bowl
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bowl $bowl)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bowl  $bowl
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bowl $bowl)
+    {
+        //
+    }
+}
